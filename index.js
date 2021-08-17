fb5d5faed77c28784d017fd9be88600bbe10c1be
Tomer <tomerir5@gmail.com>
2021-08-17 12:20:55 +0300

    changes

--- index.js
+++ index.js
@@ -1,126 +1,31 @@
-let editProfile = document.querySelector(".popout");
-let btn = document.querySelector(".profile__edit");
-let closeProfileBtn = document.getElementById('exitButton');
-let saveButton = document.getElementById('save');
-let fullName = document.getElementById('fullName').textContent
-let description = document.querySelector('.profile__subtitle').textContent
+let profileEditButton = document.getElementById('openProfile');
+let profileEditWindow = document.getElementById('popoutWindow');
+let closeProfileWindowButton = document.getElementById('closeProfileButton');
+let userFullName = document.getElementById('fullName');
+let userDescription = document.getElementById('description');
+let form = document.getElementById('form');
+let inputName = document.querySelector('.input__text_type_name');
+let inputDescription = document.querySelector('.input__text_type_description');
 
-editProfile.style.display = "none";
-function openEditProfile(){
-  if(editProfile.style.display == "none"){
-  editProfile.style.display = "block";
-  document.getElementById('id1').value=fullName; 
-  document.getElementById('id2').value=description;
+
+function openEditProfileWindow(){
+  profileEditWindow.classList.add('popout_opened');
+  inputName.value = userFullName.textContent;
+  inputDescription.value = userDescription.textContent;
 }
- else{  
-  editProfile.style.display = "none";
- }
+
+function closeEditProfileWindow(){
+  profileEditWindow.classList.remove('popout_opened')
 }
-btn.addEventListener('click', openEditProfile);
-closeProfileBtn.addEventListener('click', openEditProfile);
 
-
-
-function newSavedName(){
-  let newName = document.getElementById('id1').value;
-  document.getElementById('fullName').textContent=newName;
-  let newDescription = document.getElementById('id2').value;
-  document.querySelector('.profile__subtitle').textContent=newDescription;
-  editProfile.style.display = "none";
+function newSavedName(e) {
+  e.preventDefault();   // stop page from refrashing after submiting
+  userFullName.textContent = inputName.value;
+  userDescription.textContent = inputDescription.value;
+  profileEditWindow.classList.remove('popout_opened')
 }
-saveButton.addEventListener('click', newSavedName)
-
-let heart1 = document.getElementById("heart1")
-let filledHeart1 = document.getElementById("filledHeart1")
-let heart2 = document.getElementById("heart2")
-let filledHeart2 = document.getElementById("filledHeart2")
-let heart3 = document.getElementById("heart3")
-let filledHeart3 = document.getElementById("filledHeart3")
-let heart4 = document.getElementById("heart4")
-let filledHeart4 = document.getElementById("filledHeart4")
-let heart5 = document.getElementById("heart5")
-let filledHeart5 = document.getElementById("filledHeart5")
-let heart6 = document.getElementById("heart6")
-let filledHeart6 = document.getElementById("filledHeart6")
-
-function fillHeart1(){                   ///   each heart full function
-  heart1.style.display= "none"
-  filledHeart1.style.display = "inline"
-}
-function unfillHeart1(){
-  heart1.style.display= "inline"
-  filledHeart1.style.display = "none"
-}
-heart1.addEventListener('click', fillHeart1)
-filledHeart1.addEventListener('click', unfillHeart1)   ///
-
-function fillHeart2(){                   
-  heart2.style.display= "none"
-  filledHeart2.style.display = "inline"
-}
-function unfillHeart2(){
-  heart2.style.display= "inline"
-  filledHeart2.style.display = "none"
-}
-heart2.addEventListener('click', fillHeart2)
-filledHeart2.addEventListener('click', unfillHeart2)   
-
-function fillHeart3(){                   
-  heart3.style.display= "none"
-  filledHeart3.style.display = "inline"
-}
-function unfillHeart3(){
-  heart3.style.display= "inline"
-  filledHeart3.style.display = "none"
-}
-heart3.addEventListener('click', fillHeart3)
-filledHeart3.addEventListener('click', unfillHeart3) 
-
-function fillHeart4(){                   
-  heart4.style.display= "none"
-  filledHeart4.style.display = "inline"
-}
-function unfillHeart4(){
-  heart4.style.display= "inline"
-  filledHeart4.style.display = "none"
-}
-heart4.addEventListener('click', fillHeart4)
-filledHeart4.addEventListener('click', unfillHeart4)   
-
-function fillHeart5(){                   
-  heart5.style.display= "none"
-  filledHeart5.style.display = "inline"
-}
-function unfillHeart5(){
-  heart5.style.display= "inline"
-  filledHeart5.style.display = "none"
-}
-heart5.addEventListener('click', fillHeart5)
-filledHeart5.addEventListener('click', unfillHeart5)   
-
-function fillHeart6(){                   
-  heart6.style.display= "none"
-  filledHeart6.style.display = "inline"
-}
-function unfillHeart6(){
-  heart6.style.display= "inline"
-  filledHeart6.style.display = "none"
-}
-heart6.addEventListener('click', fillHeart6)
-filledHeart6.addEventListener('click', unfillHeart6)   
 
 
-
-
-
-
-
-
-
-/*let missingHeart = document.querySelectorAll('.elements__card_heart').forEach(function(item) {
-  item.addEventListener('click', event => {
-    item.style.display = "none"; 
-  })
-})
-
-*/
+closeProfileWindowButton.addEventListener('click', closeEditProfileWindow)
+profileEditButton.addEventListener('click', openEditProfileWindow)
+form.addEventListener('submit', newSavedName)
