328950dec9c010664175e4be8555f127c870e413
Tomer <tomerir5@gmail.com>
2021-09-23 09:45:20 +0300

    added

--- scripts/Card.js
+++ scripts/Card.js
@@ -1,15 +1,5 @@
 import {openImagePopup} from "./utils.js"
 
-const closeImage = function(evt) { 
-	const btn = evt.target; 
-	const card = btn.parentElement; 
-	card.parentElement.removeChild(card); 
-}; 
-
-const toggleLike = function(evt) {
-	evt.target.classList.toggle("card__like-active");
-};
-
 export default class Card{
 	constructor(data, template){
 		this._title = data.name;
@@ -24,14 +14,24 @@ export default class Card{
 
 		return cardElement;
 	}
+
+	_toggleLike(evt){
+		evt.target.classList.toggle("card__like-active");
+	}
+
+	_deleteCard = function(evt) { 
+		const btn = evt.target; 
+		const card = btn.parentElement; 
+		card.parentElement.removeChild(card); 
+	}; 
 	
 	generateCard(){
 		this._element = this._getTemplate();
 		this._element.querySelector(".card__image").src = this._image;
 		this._element.querySelector(".card__name").textContent = this._title;
-		this._element.querySelector('.card__like-btn').addEventListener('click', toggleLike);
+		this._element.querySelector('.card__like-btn').addEventListener('click', this._toggleLike);
 		this._element.querySelector('.card__image').addEventListener('click', openImagePopup);
-		this._element.querySelector('.card__close').addEventListener('click', closeImage);
+		this._element.querySelector('.card__close').addEventListener('click', this._deleteCard);
 		return this._element;
 	}
 }
